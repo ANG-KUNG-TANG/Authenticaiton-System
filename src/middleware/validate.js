c552f2b5bb02d8502ca4412f44b502061cae0e5a
AnriAngKung <anrikung2002@gmail.com>
2026-05-16 20:19:39 +0700

    add: add middleware of validat

---
+++ src/middleware/validate.js
@@ -0,0 +1,33 @@
+import { ValidationError} from '../utils/Errors.js'
+
+
+
+export const validate = (schema) => (req, res, next) => {
+    const result = schema.safeParse(req.body)
+    if (!result.success) {
+       const messages = Object.entries(result.erros.flatten().fieldErrors)
+        .map(([field, errors]) => `${field} : ${errors.join(", ")}`)
+        .join(' | ')
+        return next(new ValidationEror(messages))
+    }
+    req.body = result.data
+    next();
+};
+
+export const validateParams = (schema) => (req, res, next) =>{
+    const result = schema.safeParse(req.params)
+    if (!result.success){
+        return next(new ValidationError('Invalid parameters'))
+    }
+    Object.assign(req.params, result.data)
+    next()
+};
+
+export const validateQuery = (schema) => (req, res, next) => {
+    const result = schema.safeParse(req.query)
+    if (!result.success){
+        return next(new ValidationError('Invalid query paramenters'))
+    }
+    Object.assign(req.query, result.data)
+    next()
+}
